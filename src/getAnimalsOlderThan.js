e486cfc4d7d45c7c20bb4d082405d0596069a929
Alex <alexsandrolima991@hotmail.com>
2022-06-29 15:42:36 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specie = data.species.find((spec) => spec.name === animal);
+
+  return specie.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
