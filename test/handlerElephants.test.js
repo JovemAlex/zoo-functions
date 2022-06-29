00113bdcfb647ea870c831d01c8058cd2800097b
Alex <alexsandrolima991@hotmail.com>
2022-06-29 18:08:54 -0300

    requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,8 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('teste', () => {
+    expect(handlerElephants()).toBeUndefined();
+    expect(handlerElephants(1)).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+});
