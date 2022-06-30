95a8d8f48cabb7ca7bdf0c7b58f20a6330af9e46
Alex <alexsandrolima991@hotmail.com>
2022-06-30 18:30:53 -0300

    requisito 12(test)

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -2,6 +2,19 @@ const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
   it('testes', () => {
-    expect(getOpeningHours('Monday', '00:00-AM')).toEqual('The zoo is closed');
+    const openingHours = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+    expect(getOpeningHours()).toEqual(openingHours);
+    expect(getOpeningHours('')).toEqual(openingHours);
   });
 });
