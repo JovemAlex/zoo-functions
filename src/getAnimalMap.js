931e9f8bf11cc91a84617920123a4c0543d0bed6
Alex <alexsandrolima991@hotmail.com>
2022-06-30 13:51:29 -0300

    requisitos 11 e 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+const animalLocation = ({ species }) => {
+  const obj = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((acc, curr) => {
+    acc[curr.location].push(curr.name);
+    return acc;
+  }, obj);
+};
+
+function categorizedWithNames({ species }, { sorted }) {
+  const objBase = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((objeto, specie) => {
+    const objetoInterno = {};
+    objetoInterno[specie.name] = specie.residents.map((animal) => animal.name);
+    if (sorted) objetoInterno[specie.name].sort();
+    objeto[specie.location].push(objetoInterno);
+    return objeto;
+  }, objBase);
+}
+
+function categorizedBySex({ species }, { sex, sorted }) {
+  const objBase = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((objeto, specie) => {
+    const objetoInterno = {};
+    objetoInterno[specie.name] = [];
+    specie.residents.forEach((animal) => {
+      if (animal.sex === sex) objetoInterno[specie.name].push(animal.name);
+    });
+    if (sorted) objetoInterno[specie.name].sort();
+    objeto[specie.location].push(objetoInterno);
+    return objeto;
+  }, objBase);
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined || options.includeNames !== true) return animalLocation(data);
+  if (options.sex) return categorizedBySex(data, options);
+  return categorizedWithNames(data, options);
 }
 
 module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const getSpecieId = (id) => data.species.find((specie) => specie.id === id);
+
+const emptyObject = (employee) => {
+  const object = { id: '', fullName: '', species: [], locations: [] };
+  object.id = employee.id;
+  object.fullName = `${employee.firstName} ${employee.lastName}`;
+  employee.responsibleFor.forEach((id) => {
+    const specieFound = getSpecieId(id);
+    object.species.push(specieFound.name);
+    object.locations.push(specieFound.location);
+  });
+  return object;
+};
+
+const getEmployee = ({ name, id }) => {
+  let dataEmployee;
+  if (id !== undefined) {
+    dataEmployee = data.employees.find((employee) => employee.id === id);
+  } else {
+    dataEmployee = data.employees.find((employee) => {
+      const { firstName, lastName } = employee;
+      return firstName === name || lastName === name;
+    });
+  }
+  if (dataEmployee !== undefined) {
+    return emptyObject(dataEmployee);
+  }
+  throw new Error('Informações inválidas');
+};
+
+function getEmployeesCoverage(options) {
+  if (options === undefined) {
+    return data.employees.map((employee) => getEmployee({ id: employee.id }));
+  }
+  return getEmployee(options);
 }
 
 module.exports = getEmployeesCoverage;
